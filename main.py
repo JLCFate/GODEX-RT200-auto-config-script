b651f7dea3fafd10ba26e3deea4b22c407b3d075
Grzegorz Grazewicz <grzgra88@gmail.com>
2022-08-05 16:00:59 +0200

    [STRUCTURE] Extending the script with further execution levels

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from time import sleep
 
 open = True
 
-app = Application(backend='uia').start('C:\\Users\\FateStalker\\Desktop\\GoTools_V1.006\\GoConfig.exe')
+app = Application(backend='uia').start('.\\GODEX\\GoTools_V1.006\\GoConfig.exe')
 app.window(title_re='GoConfig*').wait('enabled')
 
 okno = app.window(title_re='GoConfig*')
@@ -43,7 +43,7 @@ app.kill() #myself
 
 send_keys('{LWIN down} r {LWIN up} control {SPACE} printers {ENTER}')
 
-printer_name = "Microsoft XPS Document Writer"
+printer_name = "Godex RT200"
 
 panel = Application(backend='uia')
 panel.connect(title_re='Urządzenia*', timeout=100).window(title_re='Urządzenia*').wait('enabled')
@@ -53,14 +53,50 @@ item = panel_okno.child_window(title=printer_name, control_type="ListItem").sele
 
 send_keys('{ENTER}')
 
-drukarka = panel.window(title=printer_name)
+drukarka = panel.window(title_re='Godex*')
 drukarka.wait('enabled')
 
 drukarka.child_window(title="Drukarka", control_type="MenuItem").select()
 
-send_keys('{UP} {UP} {UP} {ENTER}')
+send_keys('{UP} {UP} {ENTER}')
 
 settings = panel.window(title='Właściwości: '+printer_name)
 settings.wait('enabled')
 
 settings.child_window(title_re="Preferencje*", control_type="Button").click()
+
+preferencje = panel.window(title='Preferencje drukowania: '+printer_name)
+preferencje.wait('enabled')
+# preferencje.dump_tree()
+preferencje.child_window(title="Nowy...", control_type="Button").click()
+
+nowy_material= preferencje.window(title_re='Nowy mat*')
+nowy_material.wait('enabled')
+send_keys('{BACKSPACE} EZD {TAB} {TAB} {TAB} 50 {TAB} 30 {TAB} {ENTER}')
+
+preferencje = panel.window(title='Preferencje drukowania: '+printer_name)
+preferencje.wait('enabled')
+#preferencje.dump_tree()
+
+preferencje.child_window(title="OK", control_type="Button").click()
+
+settings.child_window(title="Narzędzia", control_type="TabItem").select()
+settings.child_window(title="Działanie", control_type="Button").click()
+send_keys('{DOWN} {DOWN} {ENTER}')
+sleep(3)
+# settings.child_window(title="Konfiguruj", control_type="Button").click()
+# send_keys('{DOWN} {ENTER}')
+# zegar = panel.active()
+# zegar.wait('enabled')
+# zegar.dump_tree()
+settings.child_window(title="Drukuj", control_type="Button").click()
+send_keys('{UP} {ENTER}')
+sleep(1)
+settings.child_window(title="Drukuj", control_type="Button").click()
+send_keys('{DOWN} {ENTER}')
+sleep(1)
+settings.child_window(title="OK", control_type="Button").click()
+
+panel.window(title_re='Godex*').close()
+panel_okno.close()
+
